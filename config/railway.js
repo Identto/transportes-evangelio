0ad74da00bacaa479c65ee419a171e92e0e22c4e
Identto <identto.digital@gmail.com>
2023-03-09 23:34:03 -0500

    Se agrega el proyecto

--- config/railway.js
+++
@@ -1,7 +0,0 @@
-
-const DB_HOST = process.env.DB_HOST || 'localhost';
-const DB_USER = process.env.DB_USER || 'root';
-const DB_PASSWORD = process.env.DB_PASSWORD || 'password';
-const DB_NAME = process.env.DB_NAME || 'transportes_evangelio';
-
-const DB_PORT = process.env.DB_PORT || '3306';
